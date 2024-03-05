e8e156392a243fdaecb8c7a7c7a412ada8625a30
oksanadocs <oksanabairachnaya@gmail.com>
2024-03-05 07:20:57 -0800

    1 commit 2 commit

--- Ht/ht_01.py
+++ Ht/ht_01.py
@@ -1,21 +1,32 @@
 def analyze_input(user_input):
-    number = user_input.replace(',', '.')
-    if number.isdigit():
-        number = int(number)
-        return (f'You enter {'negative ' if number < 0 else 'positive '}'
-                f'{'fractional' if '.' in user_input else 'integer'} number: {number}')
-    elif '.' in user_input:
-        if all(char.isdigit() or char in ['.', ','] for char in user_input):
-            number = float(number)
-            return (f'You enter {'negative' if number < 0 else 'positive'}'
-                    f'{'fractional' if '.' in user_input else 'integer'} number: {number}')
-    return f'You have entered an incorrect number: {user_input}'
+    if user_input.isdigit():
+        number = int(user_input)
+        if number == 0:
+            return "You enter 0"
+        elif number < 0:
+            return f"You enter negative {'fractional' if '.' in user_input else 'integer'} number: {number}"
+        else:
+            return f"You enter positive {'fractional' if '.' in user_input else 'integer'} number: {number}"
+    else:
+        if '.' in user_input:
+            try:
+                number = float(user_input)
+                if number == 0:
+                    return "You enter 0"
+                elif number < 0:
+                    return f"You enter negative fractional number: {number}"
+                else:
+                    return f"You enter positivefractional number: {number}"
+            except ValueError:
+                return f"You entered the wrong number: {user_input}"
+        else:
+            return f"You have entered an incorrect number: {user_input}"
 
 def main():
     while True:
-        user_input = input('Enter a number or exit to finish: ').strip().lower()
+        user_input = input("Enter a number or exit to finish: ").strip().lower()
         if user_input in ['вихід', 'exit', 'quit', 'e', 'q']:
-            print('Bye!')
+            print("Bye!")
             break
         else:
             print(analyze_input(user_input))
