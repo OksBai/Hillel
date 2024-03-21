084f0b4e236ce50b28fbd586470679ffc01fa973
oksanadocs <oksanabairachnaya@gmail.com>
2024-03-20 18:31:43 -0700

    1 commit 2 commit

--- Ht/my_project.py
+++ Ht/my_project.py
@@ -1,15 +1,21 @@
-import csv
+import csv, datetime
 
 class Database:
     def __init__(self):
         self.data = []
 
     def load_from_file(self, filename):
-        with open(filename, 'r', newline='', encoding='utf-8') as file:
-            reader = csv.reader(file)
-            for row in reader:
-                person = Person(*row)
-                self.data.append(person)
+        "Returns True if file exists, else False"
+        try:
+            with open(filename, 'r', newline='', encoding='utf-8') as file:
+                reader = csv.reader(file)
+                for row in reader:
+                    person = Person(*row)
+                    if person not in self.data:
+                        self.data.append(person)
+            return True
+        except FileNotFoundError:
+            return False
 
     def save_to_file(self, filename):
         with open(filename, 'w', newline='', encoding='utf-8') as file:
@@ -33,58 +39,77 @@ class Person:
         self.name = name
         self.surname = surname
         self.patronymic = patronymic
+        birth_date = birth_date.replace("/", ".")
+        birth_date = birth_date.replace("-", ".")
+        birth_date = birth_date.replace(" ", ".")
         self.birth_date = birth_date
+        if death_date:
+            death_date = death_date.replace("/", ".")
+            death_date = death_date.replace("-", ".")
+            death_date = death_date.replace(" ", ".")
         self.death_date = death_date
-        self.gender = gender
+        self.gender = gender.lower()
 
     def age(self):
+        """Returns age in whole years"""
         if self.birth_date is None:
-            return None
+            return 0
+        start_date = datetime.datetime.strptime(self.birth_date, "%d.%m.%Y")
+        end_date = None
         if self.death_date:
-            delta = self.death_date - self.birth_date
+            end_date = datetime.datetime.strptime(self.death_date, "%d.%m.%Y")
         else:
-            from datetime import date
-            delta = date.today() - self.birth_date
+            end_date = datetime.datetime.now()
+        delta = end_date - start_date
         return delta.days // 365
 
     def __str__(self):
         info = f'{self.name} {self.surname} {self.patronymic}'
         if self.birth_date:
-            info += f', {self.age()} years, {self.gender}. was born {self.birth_date.strftime("%d.%m.%Y")}'
+            info += f', {self.age()} years, {self.gender}. was born {self.birth_date}.'
         if self.death_date:
-            info += f'. died: {self.death_date.strftime("%d.%m.%Y")}'
+            info += f' died: {self.death_date}.'
         return info
 
 
 def main():
     database = Database()
     while True:
-        print("Menu:")
-        print("1. download from file")
+        print("\nMenu:")
+        print("1. load data from file")
         print("2. save data in file")
-        print("3. add new notice")
+        print("3. add new person to database")
         print("4. search")
         print("5. exit")
-        choice = input("choice the option: ")
+        choice = input("Choose an option: ")
 
         if choice == '1':
-            filename = input("Enter the name for download: ")
-            database.load_from_file(filename)
-            print("data downloaded")
+            filename = input("Enter CSV filename to load from: ")
+            if filename.lower().endswith(".csv"):
+                did_load = database.load_from_file(filename)
+                if did_load:
+                    print("data downloaded")
+                else:
+                    print("file does not exist")
+            else:
+                print("filename must end with .csv")
         elif choice == '2':
-            filename = input("Enter the name for save: ")
-            database.save_to_file(filename)
-            print("data saved")
+            filename = input("Enter the CSV filename to save to: ")
+            if filename.lower().endswith(".csv"):
+                database.save_to_file(filename)
+                print("data saved")
+            else:
+                print("filename must end with .csv")
         elif choice == '3':
-            name = input("enter your name: ")
-            surname = input("enter your surname: ")
-            patronymic = input("enter your father's name: ")
+            name = input("Enter person's given name: ")
+            surname = input("Enter person's surname: ")
+            patronymic = input("Enter person's father's name: ")
             birth_date = input("Enter the date of birth (format: dd.mm.yyyy): ")
             death_date = input("Enter the date of death (if applicable, format: dd.mm.yyyy): ")
             gender = input("Enter sex (m/f): ")
             person = Person(name, surname, patronymic, birth_date, death_date, gender)
             database.add_person(person)
-            print("New notice added")
+            print("New record added")
         elif choice == '4':
             query = input("Enter data to search: ")
             results = database.search(query)
@@ -98,5 +123,4 @@ def main():
             print("bye!")
             break
 
-
-
+main()
